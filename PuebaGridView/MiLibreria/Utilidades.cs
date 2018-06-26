f10f8738779e8b1601351761cde8445040c7f020
Paulo <paulosoriano.mdq@gmail.com>
2018-06-26 06:52:40 -0300

    dll

---
+++ PuebaGridView/MiLibreria/Utilidades.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace MiLibreria
+{
+    public class Utilidades
+    {
+
+        public static DataSet Ejecutar(string cmd)
+        {
+            DataSet DS;
+            MySqlDataAdapter DA;
+            MySqlConnection conexion = new MySqlConnection("server=localhost; database=ejemplo1; Uid=root; pwd=159357; SslMode=none;");
+            conexion.Open();
+
+            DS = new DataSet();
+            DA = new MySqlDataAdapter(cmd, conexion);
+
+            DA.Fill(DS);
+
+            conexion.Close();
+
+            return DS;
+
+
+        }
+    }
+}
